ccbdea0c8d96eb0de12f6ff65952de40bae02d5f
cwrlose <18932125363@163.com>
2025-03-12 16:28:51 +0800

    Update KLLoss.py

--- model-tools/KLLoss.py
+++ model-tools/KLLoss.py
@@ -1,6 +1,12 @@
-# Code for "ActionCLIP: ActionCLIP: A New Paradigm for Action Recognition"
-# arXiv:
-# Mengmeng Wang, Jiazheng Xing, Yong Liu
+"""
+logit_scale = model.logit_scale.exp()
+logits_per_image, logits_per_text = create_logits(image_embedding,text_embedding,logit_scale)#对数概率分布
+
+ground_truth = torch.tensor(gen_label(list_id),dtype=image_embedding.dtype,device=device)#普通概率分布
+loss_imgs = loss_img(logits_per_image,ground_truth)
+loss_texts = loss_txt(logits_per_text,ground_truth)
+total_loss = (loss_imgs + loss_texts)/2
+"""
 
 import torch.nn.functional as F
 import torch.nn as nn
