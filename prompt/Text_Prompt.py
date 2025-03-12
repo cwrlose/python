68e476a8891fdabc4e07ef23ca3fe3a29dc44ed3
cwrlose <18932125363@163.com>
2025-03-12 16:43:10 +0800

    Update Text_Prompt.py

--- prompt/Text_Prompt.py
+++ prompt/Text_Prompt.py
@@ -1,7 +1,11 @@
-# Code for "ActionCLIP: ActionCLIP: A New Paradigm for Action Recognition"
-# arXiv:
-# Mengmeng Wang, Jiazheng Xing, Yong Liu
+"""
+class 代表类别
 
+num_text_aug 插入长度
+
+text_dict：每个键对应的值是一个张量，包含了该文本提示模板与所有动作类别名称组合后的分词文本序列。
+这样，text_dict 就包含了所有文本提示模板与动作类别名称组合后的分词文本序列的集合
+"""
 import torch
 import clip
 
@@ -20,3 +24,10 @@ def text_prompt(data):
     classes = torch.cat([v for k, v in text_dict.items()])
 
     return classes, num_text_aug,text_dict
+
+"""
+for kkk,(images,list_id) in enumerate(tqdm(train_loader)):
+text_id = numpy.random.randint(num_text_aug,size=len(list_id))#类别长度 上限
+#text_dict[j][i, :]：根据 text_id 中的索引 j 从 text_dict 中选择对应的编码张量，再根据 list_id 中的索引 i 从该张量中选择一行。
+texts = torch.stack([text_dict[j][i,:] for i,j in zip(list_id,text_id)]
+"""
